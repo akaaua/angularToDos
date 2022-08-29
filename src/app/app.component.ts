af0d1ad2df634ab1194181ba6b864a71f83b9507
akaaua <akaaua@gmail.com>
2022-08-28 20:14:08 -0400

    Primeiro Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { Todo } from './../models/todo.model';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: String = 'Minhas Tarefas';
+  public form: FormGroup;
+
+
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+     title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+
+
+
+    this.todos.push(new Todo(1, 'Passear com o dog', false));
+    this.todos.push(new Todo(2, 'Ir ao mercado', false));
+    this.todos.push(new Todo(3, 'Aparar a juba', true));
+    this.todos.push(new Todo(4, 'Comprar almofada', false));
+  }
+
+  remove(todo: Todo){
+    const index = this.todos.indexOf(todo);
+    if(index !== -1){
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo){
+    todo.done = true;
+
+  }
+
+  markAsUndone(todo: Todo){
+    todo.done = false;
+  }
 }
---
+++ src/models/todo.model.ts
@@ -0,0 +1,10 @@
+export class Todo{
+  /**
+   *
+   */
+  constructor(
+    public id: number,
+    public title: String,
+    public done: Boolean,
+  ) {}
+}
